144f744e3782009fe9ccf893f09b6254ee100680
Arthur Kahwa <arthur_kahwa@chapter.de>
2018-02-10 20:59:04 +0100

    First set of view controllers

---
+++ Hobby Share/Hobby Share/HobbyShareViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HobbyShareViewController.swift
+//  Hobby Share
+//
+//  Created by Arthur Nsereko Kahwa on 02/10/2018.
+//  Copyright © 2018 Arthur Nsereko Kahwa. All rights reserved.
+//
+
+import UIKit
+
+class HobbyShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Hobby Share/Hobby Share/NeighborsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NeighborsViewController.swift
+//  Hobby Share
+//
+//  Created by Arthur Nsereko Kahwa on 02/10/2018.
+//  Copyright © 2018 Arthur Nsereko Kahwa. All rights reserved.
+//
+
+import UIKit
+
+class NeighborsViewController: HobbyShareViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
