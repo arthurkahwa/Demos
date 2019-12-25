dd6d6c77bfe9128c7b6014ebd892ba0b413de905
Arthur Kahwa <arthur_kahwa@chapter.de>
2019-12-25 19:32:15 +0100

    Project Setup and initial configuration

---
+++ source/Squares/Squares/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Squares
+//
+//  Created by Arthur Nsereko Kahwa on 12/19/19.
+//  Copyright © 2019 Arthur Nsereko Kahwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ source/ToDo/ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Arthur Nsereko Kahwa on 12/25/19.
+//  Copyright © 2019 Arthur Nsereko Kahwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
